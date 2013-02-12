c716918f41e3e806c979536ec7f2cc0c88c2fda2
ulises <ulises.lerm@gmail.com>
2013-02-11 17:52:59 -0800

    initial-commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Warmup::Application.config.secret_token = '4232fe223f9b63ddbba406dfe2efc3ce46649abb3de704fb2df2a18692293940b496e4f5e7dcaa4f0d2d9d0b10a9367ec63a70fa53cab75c7c3f2e274bbece05'
